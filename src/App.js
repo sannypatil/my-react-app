bb2b9e292e507689548e7a525e5f78883a592302
Patil, Santhosh <SPATIL25@volvocars.com>
2023-11-19 05:42:31 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7257/api/Todo')
+      .then(response => {
+        setTodos(response.data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => <li key={todo.id}>{todo.name}</li>)}
+      </ul>
     </div>
   );
 }
